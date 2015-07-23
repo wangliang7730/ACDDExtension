cfb8f9de350d4f29f2563c002a01a8473b6a0a0f
BunnyBlue <bunnyblueair@gmail.com>
2015-07-23 10:44:40 +0800

    new gen Resource Bridge

---
+++ buildTools/NG/AaptExt/AaptExt/CodeGenResouceBirdge.cpp
@@ -0,0 +1,9 @@
+//
+//  CodeGenResouceBirdge.cpp
+//  AaptExt
+//
+//  Created by BunnyBlue on 7/23/15.
+//  Copyright (c) 2015 BunnyBlue. All rights reserved.
+//
+
+#include "CodeGenResouceBirdge.h"
---
+++ buildTools/NG/AaptExt/AaptExt/CodeGenResouceBirdge.h
@@ -0,0 +1,14 @@
+//
+//  CodeGenResouceBirdge.h
+//  AaptExt
+//
+//  Created by BunnyBlue on 7/23/15.
+//  Copyright (c) 2015 BunnyBlue. All rights reserved.
+//
+
+#ifndef __AaptExt__CodeGenResouceBirdge__
+#define __AaptExt__CodeGenResouceBirdge__
+
+#include <stdio.h>
+
+#endif /* defined(__AaptExt__CodeGenResouceBirdge__) */
